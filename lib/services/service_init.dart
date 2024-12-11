899e1952e932a0731886fca809dc17170a9d44e2
Felix <felix@softbraincn.com>
2024-12-11 18:54:30 +0800

    去掉无用的引用

--- lib/services/service_init.dart
+++ lib/services/service_init.dart
@@ -1,8 +1,6 @@
 import 'package:inventory_app/services/data_sync.service.dart';
 import 'package:inventory_app/services/http.service.dart';
 import 'package:inventory_app/services/sqlite.service.dart';
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
 
 abstract class ServiceInit {
   static init() async {
